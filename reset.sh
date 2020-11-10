a550f839b3ff610c9bc4d5b87fe5031cc3268869
root <yuhkih@hotmail.com>
2020-11-10 12:00:16 +0900

    add reset.sh

---
+++ reset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "===== Start 1st half step ======"
+echo "rm installdir"
+rm -rf installdir/
+echo "create installdir"
+mkdir installdir
+echo "copy install-config.yaml to installdir"
+cp install-config.yaml installdir/
+echo "create manifest file" 
+./openshift-install create manifests --dir installdir
+echo "Replacing true with false in  installdir/manifests/cluster-scheduler-02-config.yml"
+sed -i -e 's/mastersSchedulable: true/mastersSchedulable: false/' installdir/manifests/cluster-scheduler-02-config.yml
+echo " ===== Start latter half of steps ====="
+echo "create ignition file"
+ ./openshift-install create ignition-configs --dir installdir
+echo "copy ignition files to ngins/html"
+\cp -f ./installdir/*.ign /usr/share/nginx/html/
+ls -ltr /usr/share/nginx/html/ | grep .ign
+echo "===== start install script ====="
+date
+ ./openshift-install --dir=installdir wait-for bootstrap-complete --log-level=debug
+echo "exit"
+exit 
+
